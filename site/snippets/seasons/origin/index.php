664ca87829cb12d6ebb62a22fd52c21d967a7e38
Eric Li <ericyoungli@gmail.com>
2020-08-22 16:43:51 -0400

    more rearranging and add nav

--- site/snippets/seasons/origin/index.php
+++ site/snippets/seasons/origin/index.php
@@ -1,4 +1,12 @@
-<div class="layout-wrapper--full">
-  <?= $season->title(); ?>
+<div class="layout-wrapper--full season--origin">
+  <div class="origin__header">
+    <div class="origin__site-title">
+      <a href="<?= $site->url(); ?>"><?= $site->full_title(); ?></a>
+    </div>
+    <div class="origin__title">
+      <?= $season->title(); ?>
+    </div>
+  </div>
+  <div class="origin__contents">
+  </div>
 </div>
-hi
