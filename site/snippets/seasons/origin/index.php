54f54171a443a26639a50942b7ec77f2dd858646
Eric Li <ericyoungli@gmail.com>
2020-08-23 21:15:00 -0400

    rearranging

--- site/snippets/seasons/origin/index.php
+++ site/snippets/seasons/origin/index.php
@@ -3,10 +3,23 @@
     <div class="origin__site-title">
       <a href="<?= $site->url(); ?>"><?= $site->full_title(); ?></a>
     </div>
-    <div class="origin__title">
-      <?= $season->title(); ?>
+    <div class="origin__definition-container expanded" id="origin-container">
+      <div class="origin__title" id="origin-title">
+        <?= $season->main_content_title(); ?>
+      </div>
+      <div class="origin__definition text">
+        <?= $season->main_content_body()->kt(); ?>
+      </div>
     </div>
   </div>
   <div class="origin__contents">
   </div>
 </div>
+
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('origin-title').addEventListener('click', (e) => {
+      document.getElementById('origin-container').classList.toggle('expanded');
+    });
+  });
+</script>
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,13 +1,10 @@
 <?php snippet('header'); ?>
-<?php snippet('nav'); ?>
+<?php snippet('nav', ['showMarquee' => $page->show_marquee()->toBool()]); ?>
 <?php if ($page->active_season()->isNotEmpty()): ?>
   <?php 
     $seasonPage = $page->active_season()->toPage();
     $seasonFile = 'seasons/' . $seasonPage->slug() . '/index'; 
     ?>
   <?php snippet($seasonFile, ['season' => $seasonPage]); ?>
-<?php endif; ?>
-<?php if ($page->show_marquee()->toBool()): ?>
-  <?php snippet('marquee'); ?>
 <?php endif; ?>
 <?php snippet('footer'); ?>
