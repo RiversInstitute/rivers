363d7a3e3e4020dede0a54239ce965e0d2bd2150
Eric Li <ericyoungli@gmail.com>
2020-09-06 18:28:39 -0400

    grid update

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,10 +1,13 @@
-<?php snippet('header'); ?>
-<?php snippet('nav', ['showMarquee' => $page->show_marquee()->toBool()]); ?>
 <?php if ($page->active_season()->isNotEmpty()): ?>
   <?php 
     $seasonPage = $page->active_season()->toPage();
     $seasonFile = 'seasons/' . $seasonPage->slug() . '/index'; 
     ?>
+  <?php snippet('header', ['headerClass' => $seasonPage->slug()]); ?>
+  <?php snippet('nav', ['showMarquee' => $page->show_marquee()->toBool()]); ?>
   <?php snippet($seasonFile, ['season' => $seasonPage]); ?>
+<?php else: ?>
+  <?php snippet('header'); ?>
+  <?php snippet('nav', ['showMarquee' => $page->show_marquee()->toBool()]); ?>
 <?php endif; ?>
 <?php snippet('footer'); ?>
