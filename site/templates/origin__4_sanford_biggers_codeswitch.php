e5a93ef09699f0f9e28b6a8b39b516aa008d9b64
Eric Li <ericyoungli@gmail.com>
2020-09-06 11:41:08 -0400

    initial interaction for biggers

--- site/templates/origin__4_sanford_biggers_codeswitch.php
+++ site/templates/origin__4_sanford_biggers_codeswitch.php
@@ -9,21 +9,30 @@
 </div>
 <ul class="codeswitch__content">
   <?php foreach($page->blocks()->toStructure() as $block): ?>
-    <li class="codeswitch__block">
+    <li 
+      class="codeswitch__block"
+      style="
+        --width: <?= rand(30, 50); ?>vw;
+        --left: <?= rand(10, 50); ?>vw;
+        --top: <?= rand(0, 50); ?>vh;
+        "
+      >
       <?php if ($block->block_type() == 'upload'): ?>
         <div class="codeswitch__block__upload">
           <?php $blockFile = $block->block_upload()->toFile(); ?>
             <?php if ($blockFile->type() == 'image'): ?>
               <img src="<?=$blockFile->resize(1000)->url(); ?>" loading="lazy">
             <?php elseif ($blockFile->type() == 'video'): ?>
-              <div class="ratio-container">
-                <video class="ratio-contained" controls playsinline loop controlslist="nodownload">
-                  <source src="<?= $blockFile->url(); ?>" type="<?= $blockFile->mime(); ?>">
-                </video>
-              </div>
+              <video controls playsinline loop controlslist="nodownload">
+                <source src="<?= $blockFile->url(); ?>" type="<?= $blockFile->mime(); ?>">
+              </video>
+            <?php elseif ($blockFile->type() == 'audio'): ?>
+              <audio controls>
+                <source src="<?= $blockFile->url(); ?>" type="<?= $blockFile->mime(); ?>">
+              </audio>
             <?php else: ?>
               <a href="<?=$blockFile->url(); ?>">
-                <div class="text">
+                <div class="text highlight">
                   <?=$blockFile->filename(); ?>
                 </div>
               </a>
@@ -31,10 +40,11 @@
         </div>
       <?php elseif ($block->block_type() == 'embed'): ?>
         <div class="codeswitch__block__embed">
+          <?= $block->block_embed(); ?>
         </div>
       <?php endif; ?>
-      <div class="codeswitch__block__text">
-        <?= $block->text()->kt(); ?>
+      <div class="codeswitch__block__text text">
+        <?= $block->block_text()->kt(); ?>
       </div>
     </li>
   <?php endforeach; ?>
@@ -44,5 +54,47 @@
   const showBio = () => {
     console.log('show bio');
   }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const blocks = document.querySelectorAll('.codeswitch__block');
+    let idx = 0;
+    document.querySelector('.codeswitch__content').addEventListener('click', () => {
+      if (idx < blocks.length) {
+        blocks[idx].style.display = 'block';
+        idx++;
+      }
+    });
+
+    blocks.forEach((el) => {
+      let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+      el.addEventListener('click', (e) => {
+        e.stopPropagation();
+      });
+
+      el.addEventListener('mousedown', (e) => {
+        e.stopPropagation();
+
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        
+        el.addEventListener('mousemove', updatePosition);
+        el.addEventListener('mouseup', () => {
+          el.querySelectorAll('div').forEach((d) => { d.style.removeProperty('pointer-events')});
+          el.removeEventListener('mousemove', updatePosition);
+        })
+      });
+
+      const updatePosition = (e) => {
+        e.preventDefault();
+        el.querySelectorAll('div').forEach((d) => { d.style.pointerEvents = 'none'});
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        el.style.top = (el.offsetTop - pos2) + "px";
+        el.style.left = (el.offsetLeft - pos1) + "px";
+      }
+    });
+  });
 </script>
 <?php snippet('footer'); ?>
