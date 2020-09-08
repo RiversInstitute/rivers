6b3d71b77736c36190e32cb1c0923d6186ef1629
Eric Li <ericyoungli@gmail.com>
2020-09-07 22:13:24 -0400

    biggers adjustments

--- site/templates/origin__4_sanford_biggers_codeswitch.php
+++ site/templates/origin__4_sanford_biggers_codeswitch.php
@@ -8,60 +8,55 @@
   </nav>
 </div>
 <ul class="codeswitch__content">
+  <?php $idx = $page->blocks()->toStructure()->count(); ?>
   <?php foreach($page->blocks()->toStructure() as $block): ?>
     <li 
       class="codeswitch__block"
       style="
-        --width: <?= rand(30, 50); ?>vw;
-        --left: <?= rand(10, 50); ?>vw;
-        --top: <?= rand(0, 50); ?>vh;
+        --width: <?= rand(20, 40); ?>vw;
+        --order: <?= $idx--; ?>
         "
       >
-      <div class="codeswitch__block__container">
-        <?php if ($block->block_type() == 'upload'): ?>
-          <div class="codeswitch__block__upload">
-            <?php $blockFile = $block->block_upload()->toFile(); ?>
-              <?php if ($blockFile->type() == 'image'): ?>
-                <img src="<?=$blockFile->resize(1000)->url(); ?>" loading="lazy">
-              <?php elseif ($blockFile->type() == 'video'): ?>
-                <video controls playsinline loop controlslist="nodownload">
-                  <source src="<?= $blockFile->url(); ?>" type="<?= $blockFile->mime(); ?>">
-                </video>
-              <?php elseif ($blockFile->type() == 'audio'): ?>
-                <audio controls>
-                  <source src="<?= $blockFile->url(); ?>" type="<?= $blockFile->mime(); ?>">
-                </audio>
-              <?php else: ?>
-                <a href="<?=$blockFile->url(); ?>">
-                  <div class="text highlight">
-                    <?=$blockFile->filename(); ?>
-                  </div>
-                </a>
-              <?php endif; ?>
-          </div>
-        <?php elseif ($block->block_type() == 'embed'): ?>
-          <div class="codeswitch__block__embed">
-            <?= $block->block_embed(); ?>
-          </div>
-        <?php endif; ?>
-        <div class="codeswitch__block__text text">
-          <?= $block->block_text()->kt(); ?>
+      <?php if ($block->block_type() == 'upload'): ?>
+        <div class="codeswitch__block__upload">
+          <?php $blockFile = $block->block_upload()->toFile(); ?>
+            <?php if ($blockFile->type() == 'image'): ?>
+              <img src="<?=$blockFile->resize(1000)->url(); ?>" loading="lazy">
+            <?php elseif ($blockFile->type() == 'video'): ?>
+              <video controls playsinline loop controlslist="nodownload">
+                <source src="<?= $blockFile->url(); ?>" type="<?= $blockFile->mime(); ?>">
+              </video>
+            <?php elseif ($blockFile->type() == 'audio'): ?>
+              <audio controls>
+                <source src="<?= $blockFile->url(); ?>" type="<?= $blockFile->mime(); ?>">
+              </audio>
+            <?php else: ?>
+              <a href="<?=$blockFile->url(); ?>">
+                <div class="text highlight">
+                  <?=$blockFile->filename(); ?>
+                </div>
+              </a>
+            <?php endif; ?>
         </div>
+      <?php elseif ($block->block_type() == 'embed'): ?>
+        <div class="codeswitch__block__embed">
+          <?= $block->block_embed(); ?>
+        </div>
+      <?php endif; ?>
+      <div class="codeswitch__block__text text">
+        <?= $block->block_text()->kt(); ?>
       </div>
     </li>
   <?php endforeach; ?>
   <li 
     class="codeswitch__block codeswitch__bio" 
     style="
-      --width: <?= rand(20, 30); ?>vw;
-      --left: var(--margin);
-      --top: var(--margin);
+      --width: <?= rand(30, 40); ?>vw;
+      --order: -1;
       "
     >
-    <div class="codeswitch__block__container">
-      <div class="codeswitch__block__text text">
-        <?= $page->bio_content()->kt(); ?>
-      </div>
+    <div class="codeswitch__block__text text">
+      <?= $page->bio_content()->kt(); ?>
     </div>
   </li>
 </ul>
@@ -83,37 +78,6 @@
       if (idx < blocksNoBio.length) {
         blocksNoBio[idx].style.display = 'block';
         idx++;
-      }
-    });
-
-    blocks.forEach((el) => {
-      let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-      el.addEventListener('click', (e) => {
-        e.stopPropagation();
-      });
-
-      el.addEventListener('mousedown', (e) => {
-        e.stopPropagation();
-
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        
-        el.addEventListener('mousemove', updatePosition);
-        el.addEventListener('mouseup', () => {
-          el.querySelectorAll('div').forEach((d) => { d.style.removeProperty('pointer-events')});
-          el.removeEventListener('mousemove', updatePosition);
-        })
-      });
-
-      const updatePosition = (e) => {
-        e.preventDefault();
-        el.querySelectorAll('div').forEach((d) => { d.style.pointerEvents = 'none'});
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        el.style.top = (el.offsetTop - pos2) + "px";
-        el.style.left = (el.offsetLeft - pos1) + "px";
       }
     });
   });
