2a9cd1aafc80b308acc3f248136fd09a2f4c12f4
Eric Li <ericyoungli@gmail.com>
2020-09-13 12:46:51 -0400

    checklist

--- site/templates/event.php
+++ site/templates/event.php
@@ -1,6 +1,6 @@
-<?php snippet('header'); ?>
+<?php snippet('header', ['headerClass' => 'event-exhibition']); ?>
 <?php snippet('nav'); ?>
-<div class="layout-wrapper event-exhibition">
+<div class="layout-wrapper">
   <div class="listing__title">
     <?= $page->title(); ?>
   </div>
@@ -9,6 +9,27 @@
   </div>
   <div class="listing__content text">
     <?= $page->main_content()->kt(); ?>
+  </div>
+</div>
+<div class="layout-wrapper--full">
+  <ul class="listing__gallery" id="gallery">
+    <?php foreach($page->main_gallery()->toFiles() as $file): ?>
+      <li class="listing__gallery__item">
+        <a href="<?= $file->resize(2000)->url() ?>">
+          <div class="listing__gallery__item__image">
+            <img src="<?= $file->resize(500)->url() ?>" loading="lazy">
+          </div>
+          <div class="listing__gallery__item__caption text">
+            <?= $file->caption()->kt(); ?>
+          </div>
+        </a>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</div>
+<div class="layout-wrapper">
+  <div class="listing__content text">
+    <?= $page->main_content_continued()->kt(); ?>
   </div>
 </div>
 <?php snippet('footer'); ?>
