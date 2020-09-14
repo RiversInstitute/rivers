95e41969d74e06aba044baf868c25146173b44fe
Eric Li <ericyoungli@gmail.com>
2020-09-14 18:36:18 -0400

    css tweaks

--- site/templates/origin__3_harryette_mullen.php
+++ site/templates/origin__3_harryette_mullen.php
@@ -2,50 +2,36 @@
 <?php snippet('nav', ['override' => $page->nav_override()->toStructure()]); ?>
 
 <div class="mullen__layout-wrapper">
-
-    <div class="poem text">
-      <?= $page->poem()->kt(); ?>
-    </div>
-
-
-    <div class="poem__citation text">
-      <?= $page->poem_citation()->kt(); ?>
-    </div>
-
-
+  <div class="poem text">
+    <?= $page->poem()->kt(); ?>
+  </div>
+  <div class="poem__citation text">
+    <?= $page->poem_citation()->kt(); ?>
+  </div>
   <ul>
-    <div class="audio">
-
-        <?php foreach($page->blocks()->toStructure() as $block): ?>
-
-          <div class="audio__caption text">
-            <?= $block->block_text()->kt(); ?>
-          </div>
-
-          <?php if ($block->block_type() == 'upload'): ?>
-
-              <?php $blockFile = $block->block_upload()->toFile(); ?>
-
-                <?php if ($blockFile->type() == 'audio'): ?>
-                  <div class="audio__block">
-                  <audio controls>
-                    <source id="myaudio" src="<?= $blockFile->url(); ?>" type="<?= $blockFile->mime(); ?>">
-                  </audio>
-                </div>
-                <?php endif; ?>
-
-          <?php elseif ($block->file_type() == 'embed'): ?>
-              <?= $block->file_embed(); ?>
-
-          <?php endif; ?>
-          <?php endforeach; ?>
+    <?php foreach($page->blocks()->toStructure() as $block): ?>
+      <li class="audio">
+        <div class="audio__caption text">
+          <?= $block->block_text()->kt(); ?>
         </div>
-    </ul>
-
-    <div class="mullen__bibliography text">
-        <?= $page->bibliography()->kt(); ?>
-    </div>
-
+        <?php if ($block->block_type() == 'upload'): ?>
+          <?php $blockFile = $block->block_upload()->toFile(); ?>
+          <?php if ($blockFile->type() == 'audio'): ?>
+            <div class="audio__block">
+              <audio controls>
+                <source id="myaudio" src="<?= $blockFile->url(); ?>" type="<?= $blockFile->mime(); ?>">
+              </audio>
+            </div>
+          <?php endif; ?>
+        <?php elseif ($block->file_type() == 'embed'): ?>
+          <?= $block->file_embed(); ?>
+        <?php endif; ?>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+  <div class="mullen__additional text">
+      <?= $page->additional_content()->kt(); ?>
+  </div>
 </div>
 
 
