8069b88e7213d0cefdf8f46e0d0e1353895c775d
Eric Li <ericyoungli@gmail.com>
2020-09-22 21:59:53 -0400

    height scroll....

--- site/templates/partners.php
+++ site/templates/partners.php
@@ -1,24 +1,26 @@
-<?php snippet('header'); ?>
+<?php snippet('header', ['headerClass' => 'no-pad']); ?>
 <?php snippet('nav'); ?>
-<div class="layout-wrapper">
-  <div class="text">
-    <?= $page->main_content()->kt(); ?>
+<div class="height-constrained">
+  <div class="layout-wrapper">
+    <div class="text">
+      <?= $page->main_content()->kt(); ?>
+    </div>
   </div>
-</div>
-<div class="layout-wrapper--full">
-  <ul class="partners">
-    <?php foreach ($page->partners()->toStructure() as $partner): ?>
-      <li class="partners__partner" style="--partner-color: <?= $partner->color(); ?>;">
-        <a href="<?= $partner->url(); ?>">
-          <div class="partner__logo">
-            <img class="partner__logo__image" loading="lazy" src="<?= $partner->logo()->toFile()->resize(500)->url(); ?>">
+  <div class="partners__container">
+    <ul class="partners">
+      <?php foreach ($page->partners()->toStructure() as $partner): ?>
+        <li class="partners__partner" style="--partner-color: <?= $partner->color(); ?>;">
+          <a href="<?= $partner->url(); ?>">
+            <div class="partner__logo">
+              <img class="partner__logo__image" loading="lazy" src="<?= $partner->logo()->toFile()->resize(500)->url(); ?>">
+            </div>
+          </a>
+          <div class="partner__description text">
+            <?= $partner->description()->kt(); ?>
           </div>
-        </a>
-        <div class="partner__description text">
-          <?= $partner->description()->kt(); ?>
-        </div>
-      </li>
-    <?php endforeach; ?>
-  </ul>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
 </div>
 <?php snippet('footer'); ?>
--- site/templates/shop.php
+++ site/templates/shop.php
@@ -1,43 +1,45 @@
-<?php snippet('header'); ?>
+<?php snippet('header', ['headerClass' => 'no-pad']); ?>
 <?php snippet('nav'); ?>
-<div class="layout-wrapper">
-  <div class="text highlight">
-    <?= $page->main_content()->kt(); ?>
+<div class="height-constrained">
+  <div class="layout-wrapper">
+    <div class="text highlight">
+      <?= $page->main_content()->kt(); ?>
+    </div>
   </div>
-</div>
-<div class="layout-wrapper--full">
-  <ul class="shop">
-    <?php foreach ($page->children()->listed() as $item): ?>
-      <li class="item">
-        <div class="item__title text">
-          <?= $item->item_title()->kt(); ?>
-        </div>
-        <?php if ($item->item_images()->toFiles()->isNotEmpty()): ?>
-          <ul class="item__images">
-            <?php $idx = 0; ?>
-            <?php foreach($item->item_images()->toFiles() as $key=>$image): ?>
-              <img class="item__images__image <?php e($idx++ == 0, 'active'); ?>" src="<?= $image->resize(500)->url(); ?>" loading="lazy">
-            <?php endforeach; ?>
-          </ul>
-        <?php endif; ?>
-        <div class="item__reviews text">
-          <?= $item->reviews()->kt(); ?>
-        </div>
-        <div class="item__content text">
-          <?= $item->main_content()->kt(); ?>
-        </div>
-        <div class="item__credits text">
-          <?= $item->credits()->kt(); ?>
-        </div>
-        <div class="item__price">
-          <?= $item->price(); ?>
-        </div>
-        <div class="item__metadata text">
-          <?= $item->metadata()->kt(); ?>
-        </div>
-      </li>
-    <?php endforeach; ?>
-  </ul>
+  <div class="layout-wrapper--full height-100">
+    <ul class="shop">
+      <?php foreach ($page->children()->listed() as $item): ?>
+        <li class="item">
+          <div class="item__title text">
+            <?= $item->item_title()->kt(); ?>
+          </div>
+          <?php if ($item->item_images()->toFiles()->isNotEmpty()): ?>
+            <ul class="item__images">
+              <?php $idx = 0; ?>
+              <?php foreach($item->item_images()->toFiles() as $key=>$image): ?>
+                <img class="item__images__image <?php e($idx++ == 0, 'active'); ?>" src="<?= $image->resize(500)->url(); ?>" loading="lazy">
+              <?php endforeach; ?>
+            </ul>
+          <?php endif; ?>
+          <div class="item__reviews text">
+            <?= $item->reviews()->kt(); ?>
+          </div>
+          <div class="item__content text">
+            <?= $item->main_content()->kt(); ?>
+          </div>
+          <div class="item__credits text">
+            <?= $item->credits()->kt(); ?>
+          </div>
+          <div class="item__price">
+            <?= $item->price(); ?>
+          </div>
+          <div class="item__metadata text">
+            <?= $item->metadata()->kt(); ?>
+          </div>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
 </div>
 <script>
   document.addEventListener('DOMContentLoaded', () => {
