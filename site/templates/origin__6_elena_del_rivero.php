55132ce6db5b8457d0368e0833046dd2bb2d3453
Eric Li <ericyoungli@gmail.com>
2020-10-23 21:16:59 -0400

    adjustments

--- site/templates/origin__6_elena_del_rivero.php
+++ site/templates/origin__6_elena_del_rivero.php
@@ -12,46 +12,46 @@
 <div class="rivero__wrapper">
 
   <div class="numbers__image">
-    <ul>
-      <button onclick="setActiveImage(idx)">
-        <?php $number = $page->image_items()->toStructure()->count(); ?>
-        <?php for ($i = 1; $i <= $number; $i++): ?>
-          <?= $i.="." ?>
-        <?php endfor; ?>
-     </button>
+    <ul class="numers__image__list">
+      <?php $number = $page->image_items()->toStructure()->count(); ?>
+      <?php for ($i = 0; $i < $number; $i++): ?>
+        <li class="numbers__image__item <?php e($i == 0, 'active'); ?>">
+          <button class="numbers__image__item__button" onclick="setActiveImage(<?= $i ?>)">
+            <?= $i+1 . "." ?>
+          </button>
+        </li>
+      <?php endfor; ?>
     </ul>
   </div>
 
-  <ul class="image__container">
+  <ul class="image__list">
     <?php $images = $page->image_items()->toStructure(); ?>
     <?php foreach($images as $image): ?>
-      <li class="image__item" id="<?= $image->slug() ?>">
+      <li class="image__item <?php e($images->indexOf($image) == 0, 'active'); ?>" id="<?= $image->id() ?>">
         <img
           src="<?= $image->image_file()->toFile()->resize(2000)->url(); ?>"
           loading="lazy"
-          class="image__item__image <?php e($images->indexOf($image) == 0, 'active'); ?>"
+          class="image__item__image"
           data-idx="<?= $images->indexOf($image) ?>"
           >
-        <div class="image__item__caption text <?php e($images->indexOf($image) == 0, 'active'); ?>">
+        <div class="image__item__caption text">
           <?= $image->caption()->kt() ?>
         </div>
       </li>
     <?php endforeach; ?>
   </ul>
 
-  <div class="text__container">
-
-    <div class="text__container__numbers">
-      <ul>
-        <button onclick="setActiveCard(idx)">
-          <?php $number = $page->text_items()->toStructure()->count(); ?>
-          <?php for ($i = 1; $i <= $number; $i++): ?>
-            <?= $i.="." ?>
-          <?php endfor; ?>
-      </button>
-      </ul>
-    </div>
-
+  <div class="text__container active">
+    <ul class="text__container__numbers">
+      <?php $number = $page->text_items()->toStructure()->count(); ?>
+      <?php for ($i = 0; $i < $number; $i++): ?>
+        <li class="text__container__number__item <?php e($i == 0, 'active'); ?>">
+          <button onclick="setActiveCard(<?= $i ?>)">
+            <?= $i+1 . "." ?>
+          </button>
+        </li>
+      <?php endfor; ?>
+    </ul>
     <ul class="text__container__text">
         <?php $texts = $page->text_items()->toStructure(); ?>
         <?php foreach($texts as $text): ?>
@@ -61,47 +61,53 @@
           </li>
         <?php endforeach; ?>
     </ul>
-
+  </div>
+  
+  <div class="text__container__toggle">
+    <button class="text__container__toggle__button active" onClick="toggleText()">T</button>
+  </div>
 </div>
 
 <script>
+  function toggleText() {
+    document.querySelector('.text__container').classList.toggle('active');
+    document.querySelector('.text__container__toggle__button').classList.toggle('active');
+  }
 
-function setActiveImage(idx) {
+  function setActiveImage(idx) {
+    const images = document.querySelectorAll('.image__item');
+    const imageNumbers = document.querySelectorAll('.numbers__image__item');
+    images.forEach((img) => img.classList.remove('active'));
+    imageNumbers.forEach((imgNum) => imgNum.classList.remove('active'));
 
-  document.querySelectorAll('.image__item').forEach((item) => {
-    item.addEventListener('click', (e) => {
-      const images = item.querySelectorAll('.image__item__image');
-      const active = item.querySelector('.image__item__image.active');
+    images[idx].classList.add('active');
+    imageNumbers[idx].classList.add('active');
+  }
 
-      const activeIdx = parseInt(active.dataset.idx);
-      const nextIdx = (activeIdx+1) % images.length;
-      console.log(activeIdx, nextIdx);
-      images[activeIdx].classList.remove('active');
-      images[nextIdx].classList.add('active');
+  function setActiveCard(idx) {
+    const cards = document.querySelectorAll('.text__item');
+    const cardNumbers = document.querySelectorAll('.text__container__number__item');
+    cards.forEach((card) => card.classList.remove('active'));
+    cardNumbers.forEach((cardNum) => cardNum.classList.remove('active'));
+
+    cards[idx].classList.add('active');
+    cardNumbers[idx].classList.add('active');
+  }
+
+  function getIndexOfImage(el) {
+    return Array.prototype.indexOf.call(el.parentNode.children, el);
+  }
+
+  document.querySelectorAll('a[href*="image"]').forEach((link) => {
+    link.addEventListener('click', () => {
+      const imageId = link.getAttribute('href');
+      const image = document.querySelector(imageId); 
+      setActiveImage(getIndexOfImage(image));
     });
   });
-}
-
-function setActiveCard(idx) {
-
-  document.querySelectorAll('.text__item.text').forEach((item) => {
-    item.addEventListener('click', (e) => {
-      const cards = item.querySelectorAll('.text__item.text');
-      const active = item.querySelector('.text__item.text.active');
-
-      const activeIdx = parseInt(active.dataset.idx);
-      const nextIdx = (activeIdx+1) % cards.length;
-      console.log(activeIdx, nextIdx);
-      cards[activeIdx].classList.remove('active');
-      cards[nextIdx].classList.add('active');
-    });
-  });
-}
 
   document.querySelector('#nav-about').addEventListener('click', () => {
     document.querySelector('.rivero__bio-credits').classList.toggle('active');
   });
-
 </script>
-
 <?php snippet('footer'); ?>
