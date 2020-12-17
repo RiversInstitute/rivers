375c8fbe18d07bb5e1b9dfd52f67b464028ce7e0
Eric Li <ericyoungli@gmail.com>
2020-12-16 19:03:19 -0800

    clean up dom

--- site/templates/origin__8_elinor_carucci.php
+++ site/templates/origin__8_elinor_carucci.php
@@ -11,46 +11,43 @@
 
 
 <div class="carucci__wrapper">
+  <ul class="image__list" id="image-list">
+    <?php $images = $page->image_items()->toStructure(); ?>
+    <?php foreach($images as $image): ?>
+      <li class="image__item">
+        <img
+          src="<?= $image->image_file()->toFile()->resize(2000)->url(); ?>"
+          loading="lazy"
+          class="image__item__image"
+          data-idx="<?= $images->indexOf($image) ?>"
+        >
+        <div class="image__item__caption text">
+          <?= $image->caption()->kt() ?>
+        </div>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</div>
 
-  <div class="scroll1">
-    <ul class="image__list">
-      <?php $images = $page->image_items()->toStructure(); ?>
-      <?php foreach($images as $image): ?>
-        <li class="image__item">
-          <img
-            src="<?= $image->image_file()->toFile()->resize(2000)->url(); ?>"
-            loading="lazy"
-            class="image__item__image"
-            data-idx="<?= $images->indexOf($image) ?>"
-            >
-          <div class="image__item__caption text">
-            <?= $image->caption()->kt() ?>
-          </div>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </div>
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+    const imageList = document.getElementById('image-list');
+    const wrapper = imageList.parentElement;
+    const windowWidth = innerWidth / 2;
 
-  <!-- <div id="scroll2">
-    <ul class="image__list">
-      <?php $images = $page->image_items()->toStructure(); ?>
-      <?php foreach($images as $image): ?>
-        <li class="image__item">
-          <img
-            src="<?= $image->image_file()->toFile()->resize(2000)->url(); ?>"
-            loading="lazy"
-            class="image__item__image"
-            data-idx="<?= $images->indexOf($image) ?>"
-            >
-          <div class="image__item__caption text">
-            <?= $image->caption()->kt() ?>
-          </div>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </div>
-</div> -->
+    imageList.addEventListener('click', () => {
+      const beforeWidth = wrapper.scrollWidth;
+      const beforeScrollPos = wrapper.scrollLeft + windowWidth;
 
 
+      imageList.classList.toggle('expanded');
 
+      const afterWidth = wrapper.scrollWidth;
+      const ratio = afterWidth / beforeWidth;
+
+      wrapper.scrollTo(beforeScrollPos * ratio - windowWidth, 0);
+
+    });
+  });
+</script>
 <?php snippet('footer'); ?>
