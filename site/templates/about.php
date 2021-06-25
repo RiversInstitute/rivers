f510fde1fa783bf1e2e9014e453a1d0f2264a75a
Eric Li <ericyoungli@gmail.com>
2021-06-25 15:18:58 -0400

    clean up about

--- site/templates/about.php
+++ site/templates/about.php
@@ -1,26 +1,16 @@
 <?php snippet('header'); ?>
 <?php snippet('nav'); ?>
-<div class="layout-wrapper__about">
-
-  <div class="about_page">
-
-    <div class="main_text">
-      <?= $page->main_content()->kt(); ?>
-    </div>
-
-    <div class="side_text">
-
-      <div class="subscribe">
-        <?= $page->subscribe()->kt(); ?>
-      </div>
-
-      <div class="colophon">
-        <?= $page->colophon()->kt(); ?>
-      </div>
-
-    </div>
-
+<div class="about__container">
+  <div class="text">
+    <?= $page->main_content()->kt(); ?>
   </div>
-
+  <div class="about__rail">
+    <div class="text about__rail__primary">
+      <?= $page->right_rail_1()->kt(); ?>
+    </div>
+    <div class="text about__rail__secondary">
+      <?= $page->right_rail_2()->kt(); ?>
+    </div>
+  </div>
 </div>
 <?php snippet('footer'); ?>
