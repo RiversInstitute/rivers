13353b0b7276f8d9ff0e60aca9c316efbbaf4c35
Eric Li <ericyoungli@gmail.com>
2021-06-27 11:37:52 -0400

    refactor css and remove origin

--- site/templates/season.php
+++
@@ -1,5 +0,0 @@
-<?php snippet('header'); ?>
-<?php snippet('nav'); ?>
-<?php $seasonFile = 'seasons/' . $page->slug() . '/index'; ?>
-<?php snippet($seasonFile, ['season' => $page]); ?>
-<?php snippet('footer'); ?>
