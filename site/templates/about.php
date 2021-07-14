b16d120ed505bab3321de98bc8be57b8703241c3
Eric Li <ericyoungli@gmail.com>
2021-07-13 22:23:52 -0400

    rivers about updates

--- site/templates/about.php
+++ site/templates/about.php
@@ -4,13 +4,8 @@
   <div class="text">
     <?= $page->main_content()->kt(); ?>
   </div>
-  <div class="about__rail">
-    <div class="text about__rail__primary">
-      <?= $page->right_rail_1()->kt(); ?>
-    </div>
-    <div class="text about__rail__secondary">
-      <?= $page->right_rail_2()->kt(); ?>
-    </div>
+  <div class="text about__rail">
+    <?= $page->right_rail()->kt(); ?>
   </div>
 </div>
 <?php snippet('footer'); ?>
