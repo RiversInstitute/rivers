764e57b94ebd7e70dd7c46bee87d940bb603ac19
Eric Li <ericyoungli@gmail.com>
2021-07-14 20:53:09 -0400

    line breaks

--- site/snippets/donate.php
+++ site/snippets/donate.php
@@ -1,13 +1,3 @@
 <script src="https://donorbox.org/widget.js" paypalExpress="true"></script>
 
-<iframe 
-  src="https://donorbox.org/embed/make-a-donation-156" 
-  name="donorbox" 
-  allowpaymentrequest="" 
-  seamless="seamless" 
-  frameborder="0" 
-  scrolling="no" 
-  height="900px" 
-  width="100%" 
-  style="min-width: 250px; max-height:none!important;">
-</iframe>
+<iframe src="https://donorbox.org/embed/make-a-donation-156" name="donorbox" allowpaymentrequest="" seamless="seamless" frameborder="0" scrolling="no" height="900px" width="100%" style="min-width: 250px; max-height:none!important"></iframe>
