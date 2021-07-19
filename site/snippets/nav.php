6f5d8a73e2c1e241d7a47bda0cacdf8d0052d58e
Eric Li <ericyoungli@gmail.com>
2021-07-18 22:38:37 -0400

    nav update

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -11,13 +11,15 @@ if (isset($override)) {
   <?php if (isset($showMarquee) && $showMarquee): ?>
     <?php snippet('marquee'); ?>
   <?php endif; ?>
-  <nav>
-    <ul class="navigation">
-      <li class="navigation__item <?php e($site->homePage()->isOpen() && !isset($override), 'highlight') ?>"><a href="<?= $site->url(); ?>"><?= $site->title(); ?></a></li>
-      <?php foreach($items as $item): ?>
-      <li id="nav-<?= $item->title()->slug(); ?>" class="navigation__item <?php e($item->isOpen() && !isset($override), 'highlight') ?>"><a href="<?= $item->url() ?>"><?= $item->title(); ?></a></li>
-      <?php endforeach ?>
-    </ul>
-    <?php snippet('email-signup'); ?>
-  </nav>
+  <div class="navigation__wrapper">
+    <nav>
+      <ul class="navigation">
+        <li class="navigation__item <?php e($site->homePage()->isOpen() && !isset($override), 'highlight') ?>"><a href="<?= $site->url(); ?>"><?= $site->title(); ?></a></li>
+        <?php foreach($items as $item): ?>
+        <li id="nav-<?= $item->title()->slug(); ?>" class="navigation__item <?php e($item->isOpen() && !isset($override), 'highlight') ?>"><a href="<?= $item->url() ?>"><?= $item->title(); ?></a></li>
+        <?php endforeach ?>
+      </ul>
+      <?php snippet('email-signup'); ?>
+    </nav>
+  </div>
 </div>
