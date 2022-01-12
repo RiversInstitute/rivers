579b9a5ac9db48fa219d4a25cd4b62887d5ef445
Eric Li <ericyoungli@gmail.com>
2022-01-11 19:12:12 -0800

    update resert and grid on mobile

--- site/snippets/associated-grid.php
+++ site/snippets/associated-grid.php
@@ -1,9 +1,16 @@
 <?php
   srand(mktime(0, 0, 0));
   $positions = [];
+  $positionsMobile = [];
+  
   $width = 6;
+  $widthMobile = 2;
   $height = size($contents);
+  $heightMobile = size($contents) * 2;
+
   $coords = array_fill(0, $width * $height, 0);
+  $coordsMobile = array_fill(0, $widthMobile * $heightMobile, 0);
+  
   foreach($contents as $idx=>$entry) {
     $x = rand(0, $width-1);
     $y = rand(0, $height-1);
@@ -14,6 +21,17 @@
     $coords[$x*$y] = 1;
     array_push($positions, [$x+1, $y+1]);
   }
+
+  foreach($contents as $idx=>$entry) {
+    $x = rand(0, $widthMobile-1);
+    $y = rand(0, $heightMobile-1);
+    while ($coords[$x*$y] != 0) {
+      $x = rand(0,$widthMobile-1);
+      $y = rand(0,$heightMobile-1);
+    }
+    $coordsMobile[$x*$y] = 1;
+    array_push($positionsMobile, [$x+1, $y+1]);
+  }
   srand();
 ?>
 <ul 
@@ -22,6 +40,8 @@
   style="
     --width--count: <?= $width; ?>;
     --height--count: <?= $height; ?>;
+    --width--count--mobile: <?= $widthMobile; ?>;
+    --height--count--mobile: <?= $heightMobile; ?>;
   "
   >
   <?php foreach($contents as $idx=>$content): ?>
@@ -29,6 +49,7 @@
       class="associated__grid__item"
       style="
         --grid-area: <?= $positions[$idx][1]; ?> / <?= $positions[$idx][0]; ?>;
+        --grid-area--mobile: <?= $positionsMobile[$idx][1]; ?> / <?= $positionsMobile[$idx][0]; ?>;
       "
       >
       <?php if($content->links_to() == "page"): ?>
