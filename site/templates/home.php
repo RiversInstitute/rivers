6587a6597535cb59e311f178cd14283bf5f189a0
Elliott Cost <email@elliott.computer>
2025-02-11 13:15:57 +0100

    mobile ticker

--- site/templates/home.php
+++ site/templates/home.php
@@ -7,18 +7,19 @@
     </a>
   </div> -->
 
+  <!-- Ticker path -->
   <div class="home__ticker">
-      <svg width="1244" height="483" viewBox="0 0 1244 483" xmlns="http://www.w3.org/2000/svg">
-          <path id="path" d="M0 0.5H1229.5C1237.23 0.5 1243.5 6.76801 1243.5 14.5V143C1243.5 150.732 1237.23 157 1229.5 157H14.5C6.76801 157 0.5 163.268 0.5 171V286C0.5 293.732 6.76801 300 14.5 300H1229.5C1237.23 300 1243.5 306.268 1243.5 314V468C1243.5 475.732 1237.23 482 1229.5 482H14.5C6.76801 482 0.5 475.732 0.5 468V14.5C0.5 6.76801 6.76801 0.5 14.5 0.5H0Z" fill="none" stroke="black"/>
-          <text>
-              <textPath href="#path" id="marquee1" startOffset="0%" side="right">
-                  Rivers Institute for Contemporary Art & Thought (Rivers) is a non-profit institute for research and publishing, exhibitions and convenings on art of the global diaspora.
-              </textPath>
-              <textPath href="#path" id="marquee2" startOffset="100%" side="right">
-                  Rivers Institute for Contemporary Art & Thought (Rivers) is a non-profit institute for research and publishing, exhibitions and convenings on art of the global diaspora.
-              </textPath>
-          </text>
-      </svg>
+    <svg width="1244" height="483" viewBox="0 0 1244 483" xmlns="http://www.w3.org/2000/svg">
+        <path id="path" d="M0 0.5H1229.5C1237.23 0.5 1243.5 6.76801 1243.5 14.5V143C1243.5 150.732 1237.23 157 1229.5 157H14.5C6.76801 157 0.5 163.268 0.5 171V286C0.5 293.732 6.76801 300 14.5 300H1229.5C1237.23 300 1243.5 306.268 1243.5 314V468C1243.5 475.732 1237.23 482 1229.5 482H14.5C6.76801 482 0.5 475.732 0.5 468V14.5C0.5 6.76801 6.76801 0.5 14.5 0.5H0Z" fill="none" stroke="black"/>
+        <text>
+            <textPath href="#path" id="marquee1" startOffset="0%" side="right">
+                Rivers Institute for Contemporary Art & Thought (Rivers) is a non-profit institute for research and publishing, exhibitions and convenings on art of the global diaspora.
+            </textPath>
+            <textPath href="#path" id="marquee2" startOffset="100%" side="right">
+                Rivers Institute for Contemporary Art & Thought (Rivers) is a non-profit institute for research and publishing, exhibitions and convenings on art of the global diaspora.
+            </textPath>
+        </text>
+    </svg>
   </div>
 
   <div class="home__contents">
@@ -244,11 +245,24 @@
 
     function updateFontSize() {
         const text = document.querySelector('text');
+        const svg = document.querySelector('.home__ticker svg');
+        const path = document.querySelector('.home__ticker path');
         const screenWidth = window.innerWidth;
-        if (screenWidth <= 1244) {
-            text.style.fontSize = `${(1244 / screenWidth) * 26}px`;
+
+        if (screenWidth <= 520) {
+            // Mobile path and viewBox
+            svg.setAttribute('viewBox', '0 0 320 371');
+            svg.setAttribute('width', '320');
+            svg.setAttribute('height', '371');
+            path.setAttribute('d', 'M319 106.935V15C319 7.26801 312.732 1 305 1H296.553H15C7.26801 1 1 7.26801 1 15V356C1 363.732 7.26802 370 15 370H305C312.732 370 319 363.732 319 356V244.523C319 236.791 312.732 230.523 305 230.523H91.9981C84.2661 230.523 77.9981 224.255 77.9981 216.523V134.935C77.9981 127.203 84.2661 120.935 91.9981 120.935H305C312.732 120.935 319 114.667 319 106.935Z');
+            text.style.fontSize = '20px'; // Fixed size for mobile
         } else {
-            text.style.fontSize = '26px';
+            // Desktop path and viewBox
+            svg.setAttribute('viewBox', '0 0 1244 483');
+            svg.setAttribute('width', '1244');
+            svg.setAttribute('height', '483');
+            path.setAttribute('d', 'M0 0.5H1229.5C1237.23 0.5 1243.5 6.76801 1243.5 14.5V143C1243.5 150.732 1237.23 157 1229.5 157H14.5C6.76801 157 0.5 163.268 0.5 171V286C0.5 293.732 6.76801 300 14.5 300H1229.5C1237.23 300 1243.5 306.268 1243.5 314V468C1243.5 475.732 1237.23 482 1229.5 482H14.5C6.76801 482 0.5 475.732 0.5 468V14.5C0.5 6.76801 6.76801 0.5 14.5 0.5H0Z');
+            text.style.fontSize = screenWidth <= 1244 ? `${(1244 / screenWidth) * 26}px` : '26px';
         }
     }
 
@@ -256,7 +270,7 @@
     updateFontSize(); // Initial call
 
     function animateMarquee() {
-        offset += speed;
+        offset += speed; // Adjust speed here
         if (offset > 100) offset -= 100; // Wrap around without resetting to 0
 
         marquee1.setAttribute("startOffset", `${offset}%`);
