b9da00d928a3ba4064a94c4f36815c7c8ea7e74f
Elliott Cost <email@elliott.computer>
2025-03-15 23:38:37 +0100

    artists pages

--- site/templates/residency.php
+++ site/templates/residency.php
@@ -1,8 +1,8 @@
 <?php snippet('header', ['headerClass' => 'height-limited']); ?>
 <?php snippet('nav'); ?>
 <div class="subsection-nav-wrapper">
-    <div class="subsection-nav" style="height: 80dvh;">
-        <?php snippet('subsection-grid-six', ['grid_items' => $page->shelf()->toPages(), 'nav' => false, 'images' => true]); ?>
+    <div class="subsection-nav" style="height: 80dvh;">//
+        <?php snippet('subsection-grid-six', ['grid_items' => $page->shelf()->toPages(), 'nav' => false, 'images' => true, 'color' => $page->color()]); ?>
     </div>
 
     <div class="subsection-index-container">
